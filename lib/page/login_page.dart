9175ead918c56ba3ba78f446b775ef6fde2f6e7b
Artur1im <lim.artur17@gmail.com>
2024-03-04 18:44:14 +0600

    made a login page

---
+++ lib/page/login_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:messenger/widget/my_button.dart';
+import 'package:messenger/widget/my_text_field.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                const Icon(Icons.message, size: 80),
+                const SizedBox(height: 50),
+                const Text('Welcome back you'),
+                const SizedBox(height: 50),
+                MTF(
+                    controller: emailController,
+                    hintText: 'Email',
+                    obscureText: false),
+                const SizedBox(height: 50),
+                MTF(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: true),
+                const SizedBox(height: 50),
+                MyButton(onTap: () {}, text: "Sign In")
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widget/my_text_field.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MTF extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+  const MTF(
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      required this.obscureText});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            hintText: hintText,
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade200),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade200),
+            ),
+            fillColor: Colors.grey[400],
+            filled: true,
+            hintStyle: const TextStyle(color: Colors.white)));
+  }
+}
