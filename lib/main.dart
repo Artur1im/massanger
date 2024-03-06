dee33b3e86b538a7461770615af92e46ba24a742
Artur1im <lim.artur17@gmail.com>
2024-03-06 17:38:05 +0600

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:messenger/services/auth/login_or_register.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyB5tfqDMei1a7RgPeSd8eL106rEF8FiZmU",
+      appId: "1:540215271818:web:8b22d4aee01acdce862873",
+      messagingSenderId: "1041630790571",
+      projectId: "messenger-32a83",
+    ),
+  );
   runApp(const MyApp());
 }
 
